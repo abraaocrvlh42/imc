39deefb1cad5c24f4ed568d11c3246ea43397b3d
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-20 01:45:08 -0300

    calculations controller

--- app/controllers/api/v1/calculations_controller.rb
+++ app/controllers/api/v1/calculations_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::CalculationsController < ApplicationController
   def imc
-    render json: {message: "IMC routes request with success"}
+    puts "*" * 100
+    puts params
+
+    name   = params[:name]
+    age    = params[:age]
+    height = params[:height]
+    weight = params[:weight]
+
+    result = (weight / ( height ** 2 )).round(2)
+
+    render json: {message: "IMC is #{result}"}
   end
 end
