ee61a0b7ee95074ff725aea8e05e9dd3ac9ce1eb
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-20 12:23:44 -0300

    BMI calculation

--- app/controllers/api/v1/calculations_controller.rb
+++ app/controllers/api/v1/calculations_controller.rb
@@ -1,15 +1,49 @@
 class Api::V1::CalculationsController < ApplicationController
   def imc
-    puts "*" * 100
-    puts params
-
-    name   = params[:name]
-    age    = params[:age]
-    height = params[:height]
-    weight = params[:weight]
-
+    name = params[:name]
+    age = params[:age]
+    height = params[:height] # numeros decimais
+    weight = params[:weight] # numeros decimais
+  
     result = (weight / ( height ** 2 )).round(2)
-
-    render json: {message: "IMC is #{result}"}
+  
+    render json: {
+      imc: result,
+      classification: set_classification(result),
+      obesity: obesity_level(result)
+  }
+  end
+  
+  private
+  
+  def set_classification(result)
+     # aqui seria a logica usando if ou melhor usando case para definir a classificação de acorto com o resultado do calculo IMC
+    case result
+    when 18.5
+       "Under weight"
+    when 18.5..25.0
+       "Normal weight"
+    when 25.0..29.9
+       "Overweight"
+    when 30.0..34.9
+       "Obesity level I"
+    when 35.0..39.9
+       "Obesity level II"
+    when 40
+       "Greater than or equal to 40 is obesity III"      
+    end
+  end
+  
+  def obesity_level(result)
+    case result
+    when 30.0..34.9 
+      "I"
+    when 35.0..39.9
+      "II"
+    when 40
+      "III"
+    else
+      "-"
+    end
   end
 end
