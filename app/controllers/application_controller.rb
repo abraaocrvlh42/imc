b1f0a6a80658945c523825ce67b525394f2d097f
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-01-21 21:11:03 -0300

    Methods for authorization to use the api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::API
     render json: { error: 'not_found' }
   end
 
+# Request authorization method
+
   def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
     begin
       @decoded = JsonWebToken.decode(header)
-      @current_user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
